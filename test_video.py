f3928e69313ce98e7c55c5055b8617c143e4352c
Eric Wang <eric.wang2237@gmail.com>
2022-10-01 18:03:50 -0400

    testing file

--- test_video.py
+++ test_video.py
@@ -1,11 +1,13 @@
-
 import vimeo
 
 client = vimeo.VimeoClient(
-    token='c036cd8338653e5c7cf87d92b4660ea4',
+    token='92921aaaaa5a045bda497dff39a51bc9',
     key='dee2969f3618385d60e0b6d50e9f799b2990ddbc',
     secret='sUYsN6kOMQrnYHOaFEri3WDOVo/U49WSIDmPe27LheUa61Vsn1ckKjddGMcW0i0mpDasNSgQVAILqeS2lJOshqEmMAICfHWgbZpJyTQzi6Oj7rOSZjuJ+ff/1SMg5y/X'
   )
 
-response = client.get(uri)
-print(response.json())
+about_me = client.get('/me', params={"fields": "uri,name,pictures"})
+
+# https://vimeo.com/747752214, "Chains of Habit"
+video = client.get('https://api.vimeo.com/videos/747752214')
+print(video.json()['name'])
