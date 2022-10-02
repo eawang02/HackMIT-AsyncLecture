c149b291f90441b8929f5b43a053090c372bef3e
munirsidd <munirsid@bu.edu>
2022-10-02 00:00:22 -0400

    updating

--- static/JS.js
+++ static/JS.js
@@ -1,3 +1,9 @@
 function hide_qs() {
-    document.getElementById("question_response").style.visibility = "hidden";
+    document.getElementById("question_response").style.display = "none";
 }
+
+function show_qs() {
+    document.getElementById("question_response").style.display = "block";
+}
+ 
+hide_qs();
