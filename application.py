0da60c8f4d29a165eebf9c0cd0e364303f10c21a
Eric Wang <eric.wang2237@gmail.com>
2022-10-01 21:39:56 -0400

    Add functionality for website to post questions to backend

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -13,3 +14,13 @@ def after_request(response):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/test', methods=['POST'])
+def test():
+    output = request.get_json()
+    print(output) # This is the output that was stored in the JSON within the browser
+    print(type(output))
+    result = json.loads(output) #this converts the json output to a python dictionary
+    print(result) # Printing the new dictionary
+    print(type(result))#this shows the json converted as a python dictionary
+    return result
