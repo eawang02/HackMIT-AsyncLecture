7eb80ef74365d2477611eac96d1d7d8b26b76e04
Eric Wang <eric.wang2237@gmail.com>
2022-10-01 23:45:05 -0400

    Implement storing and retrieving question data

--- application.py
+++ application.py
@@ -13,6 +13,7 @@ def after_request(response):
 
 @app.route("/")
 def index():
+    refresh()
     return render_template("index.html")
 
 @app.route('/test', methods=['POST'])
@@ -23,4 +24,28 @@ def test():
     result = json.loads(output) #this converts the json output to a python dictionary
     print(result) # Printing the new dictionary
     print(type(result))#this shows the json converted as a python dictionary
+
+    saveQuestion(output)
     return result
+
+def refresh():
+    question_file = open(r"questions.txt", "r")
+    raw = question_file.readlines()
+    questions = []
+
+    for x in raw:
+        questions.append(json.loads(x))    
+
+    questions = sorted(questions, key=lambda i: i['time'])
+    print(questions)
+
+    return
+
+def saveQuestion(q):
+    question_file = open(r"questions.txt", "a+")
+    question_dict = json.loads(q)
+
+    question_file.write(json.dumps(question_dict) + "\n")
+
+    question_file.close()
+    return
