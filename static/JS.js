725968b967fccf7c3361691d856c9432eab8fb98
munirsidd <munirsid@bu.edu>
2022-10-02 10:11:11 -0400

    updating

--- static/JS.js
+++ static/JS.js
@@ -1,4 +1,4 @@
-function hide_qs() {
+/* function hide_qs() {
     document.getElementById("question_response").style.display = "none";
 }
 
@@ -7,3 +7,10 @@ function show_qs() {
 }
 
 hide_qs();
+*/
+
+
+$("#question_response").fadeOut();
+
+$("#question_response").fadeIn();
+
